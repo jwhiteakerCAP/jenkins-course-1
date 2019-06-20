d4f5fffb84dcfe939e807d4cf5004f22647796bc
Joe Whiteaker <48360741+jwhiteakerCAP@users.noreply.github.com>
2019-06-20 14:07:02 -0500

    Update nodejs.groovy

--- job-dsl/nodejs.groovy
+++ job-dsl/nodejs.groovy
@@ -1,8 +1,8 @@
 job('NodeJS example') {
     scm {
-        git('git://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+        git('git://git@github.com:jwhiteakerCAP/jenkins-course-1.git') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL User')
-            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+            node / gitConfigEmail('deadhousediaries@gmail.com')
         }
     }
     triggers {
